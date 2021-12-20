1d6e02a022fa827e9fca926b8d44c85cb90a33c7
zhen <770226915@qq.com>
2021-12-20 11:53:53 +0800

    09

---
+++ 09/09.ts
@@ -0,0 +1,24 @@
+{
+  enum Day {
+    SUNDAY,
+    MONDAY,
+    THUSDAY,
+    WENDESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+  }
+  //   转译后
+  //   var Day = void 0;
+  //   (function (Day) {
+  //     Day[(Day["SUNDAY"] = 0)] = "SUNDAY";
+  //     Day[(Day["MONDAY"] = 1)] = "MONDAY";
+  //     Day[(Day["TUESDAY"] = 2)] = "TUESDAY";
+  //     Day[(Day["WEDNESDAY"] = 3)] = "WEDNESDAY";
+  //     Day[(Day["THURSDAY"] = 4)] = "THURSDAY";
+  //     Day[(Day["FRIDAY"] = 5)] = "FRIDAY";
+  //     Day[(Day["SATURDAY"] = 6)] = "SATURDAY";
+  //   })(Day || (Day = {}));
+}
+
+
